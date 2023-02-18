1c5a9d585a6c6bf58cca7fd16e3f2d97dde78905
Your Name~git <you@example.com>
2023-02-18 15:18:19 +0700

    создание HW03 решение 3-ей задачи

---
+++ HW03/Program.cs
@@ -0,0 +1,46 @@
+﻿// Напишите программу, которая принимает на входе цифру,
+// обозначающую день недели.
+// Выяснить является номер дня недели выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int Prompt(string message)
+{
+  Console.Write(message);
+  string value = Console.ReadLine();
+  int result = Convert.ToInt32(value);
+  return result;
+}
+
+bool IsWeekend(int weekDay)
+{
+  if (weekDay > 5)
+  {
+    return true;
+  }
+  return false;
+}
+
+bool ValidateWeekday(int number)
+{
+  if (number > 0 && number <= 7)
+  {
+    return true;
+  }
+  Console.WriteLine("Такого дня недели нет!");
+  return false;
+}
+
+int weekDay = Prompt("Введите цифру дня недели");
+if (ValidateWeekday(weekDay))
+{
+  if (IsWeekend(weekDay))
+  {
+    Console.WriteLine("Выходной");
+  }
+  else
+  {
+    Console.WriteLine("Рабочий день");
+  }
+}
